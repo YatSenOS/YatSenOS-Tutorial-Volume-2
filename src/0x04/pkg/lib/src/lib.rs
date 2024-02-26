940c1395d0c9adc54cdae6cd817b56d363baee07
GZTime <Time.GZ@outlook.com>
2024-02-26 12:36:45 +0800

    chore(lab/4): allow unused_imports for user lib

--- src/0x04/pkg/lib/src/lib.rs
+++ src/0x04/pkg/lib/src/lib.rs
@@ -1,7 +1,7 @@
-#![allow(dead_code)]
+#![cfg_attr(not(test), no_std)]
+#![allow(dead_code, unused_imports)]
 #![feature(panic_info_message)]
 #![feature(alloc_error_handler)]
-#![cfg_attr(not(test), no_std)]
 
 #[macro_use]
 pub mod macros;
