94bcded567b3d8a0678d2a6d73cfc520dcabc7de
GZTime <Time.GZ@outlook.com>
2024-02-26 19:18:42 +0800

    wip(lab/5): add code

---
+++ src/0x05/pkg/lib/src/sync.rs
@@ -0,0 +1,50 @@
+use core::{
+    hint::spin_loop,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+use crate::*;
+
+pub struct SpinLock {
+    bolt: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self {
+            bolt: AtomicBool::new(false),
+        }
+    }
+
+    pub fn acquire(&mut self) {
+        // FIXME: acquire the lock, spin if the lock is not available
+    }
+
+    pub fn release(&mut self) {
+        // FIXME: release the lock
+    }
+}
+
+unsafe impl Sync for SpinLock {} // Why?
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Semaphore {
+    /* FIXME: record the sem key */
+}
+
+impl Semaphore {
+    pub const fn new(key: u32) -> Self {
+        Semaphore { key }
+    }
+
+    // FIXME: inline functions with syscall
+}
+
+unsafe impl Sync for Semaphore {}
+
+#[macro_export]
+macro_rules! semaphore_array {
+    [$($x:expr),+ $(,)?] => {
+        [ $($crate::Semaphore::new($x),)* ]
+    }
+}
