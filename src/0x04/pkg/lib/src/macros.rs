343c542ea574b634273e2002faaeb06a9334e9d2
GZTime <Time.GZ@outlook.com>
2025-03-10 23:38:39 +0800

    fix(lab/4): panic handler

--- src/0x04/pkg/lib/src/macros.rs
+++ src/0x04/pkg/lib/src/macros.rs
@@ -17,7 +17,7 @@ macro_rules! entry {
 fn panic(info: &core::panic::PanicInfo) -> ! {
     let location = if let Some(location) = info.location() {
         alloc::format!(
-            "{}:{}:{}",
+            "{}@{}:{}",
             location.file(),
             location.line(),
             location.column()
@@ -25,12 +25,12 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     } else {
         "Unknown location".to_string()
     };
-    let msg = if let Some(msg) = info.message() {
-        alloc::format!("{}", msg)
-    } else {
-        "No more message...".to_string()
-    };
-    errln!("\n\n\rERROR: panicked at {}\n\n\r{}", location, msg);
+
+    errln!(
+        "\n\n\rERROR: panicked at {}\n\n\r{}",
+        location,
+        info.message()
+    );
 
     // FIXME: after syscall, add lib::sys_exit(1);
     loop {}
